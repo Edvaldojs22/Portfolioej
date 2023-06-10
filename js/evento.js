5ff1fc099a8b65000563fc67540653ab3630d9f4
Edvaldojs22 <124460629+Edvaldojs22@users.noreply.github.com>
2023-06-10 16:19:00 -0300

    atualizacao

--- js/evento.js
+++ js/evento.js
@@ -3,15 +3,28 @@
 let texto = "Desenvolvedor Fronte End"
 const velocidade = 200
 
-const textoElemento = document.querySelector('.texto-desenvolvedor') 
+const textoElemento = document.querySelector('.texto-desenvolvedor')
 
-function exibeLetra (texto, index) {
+function exibeLetra(texto, index) {
     textoElemento.textContent = texto.slice(0, index);
 
     if (index < texto.length) {
-        setTimeout(() => exibeLetra(texto, index + 1),velocidade)
+
+        setTimeout(() => exibeLetra(texto, index + 1), velocidade)
     }
-     
+
+    else if (index == texto.length) {
+        setTimeout(() => {
+            texto = textoElemento.textContent = ""
+        }, 4000)
+
+        setTimeout(() => {
+            texto = textoElemento.textContent = "Desenvolvedor Front End"
+            exibeLetra(texto, 0)
+        }, 4000)
+
+    }
+
 }
 
 exibeLetra(texto, 0)
@@ -26,18 +39,33 @@ const caixaDois = document.querySelector('.dois');
 const caixaTres = document.querySelector('.tres');
 const caixaQuatro = document.querySelector('.quatro');
 const caixaCinco = document.querySelector('.cinco');
-const caixaEvento = document.querySelector('.evento')
+const caixaEvento = document.querySelector('.evento');
+let teste = true
 
 
-caixaEvento.addEventListener('click', () => {[
-    caixaEvento.style.display = "none"
-]
-    caixaUm.classList.add('caixaUm');
-    caixaDois.classList.add('caixaDois');
-    caixaTres.classList.add('caixaTres');
-    caixaQuatro.classList.add('caixaQuatro');
-    caixaCinco.classList.add('caixaCinco');
-   
+caixaEvento.addEventListener('click', () => {
+    if (teste) {
+        caixaEvento.style.width = "0"
+        caixaEvento.style.height = "0"
+        caixaUm.classList.add('caixaUm');
+        caixaDois.classList.add('caixaDois');
+        caixaTres.classList.add('caixaTres');
+        caixaQuatro.classList.add('caixaQuatro');
+        caixaCinco.classList.add('caixaCinco');
+        teste = false
+    } else{
+        caixaEvento.style.width = "100px"
+        caixaEvento.style.height = "100px"
+        caixaUm.classList.remove('caixaUm');
+        caixaDois.classList.remove('caixaDois');
+        caixaTres.classList.remove('caixaTres');
+        caixaQuatro.classList.remove('caixaQuatro');
+        caixaCinco.classList.remove('caixaCinco');
+        teste = true
+
+    }
+
+
 })
 //------------------------------------------------------------------------------
 
