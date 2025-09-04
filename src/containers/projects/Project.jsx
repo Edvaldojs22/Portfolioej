85a0e7e3283c567735d8503e74b8854a9aed5977
Edvaldojs22 <jonas.edvaldo.10@gmail.com>
2025-09-04 20:26:44 -0300

    feat: add Project component with styles for layout and skills display

---
+++ src/containers/projects/Project.jsx
@@ -0,0 +1,25 @@
+import "./Project.css";
+export default function Project({ img, h1text, text, technologies }) {
+  return (
+    <div className="opacity_projeto">
+      <div className="img_texto_sobre">
+        <img src={img} alt="" />
+        <h1>{h1text}</h1>
+      </div>
+
+      <div className="texto_sobre">
+        <div className="painel_text_p">
+          <p>{text}</p>
+        </div>
+
+        <ul className="painel_skills">
+          {technologies.map((TecIcon, index) => (
+            <li key={index}>
+              <TecIcon />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
